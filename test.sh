09caae077498dba86b385f23d7ec080dab98e66f
Nick Robinson <nickthecoder@gmail.com>
2013-07-28 21:28:30 +0100

    test.sh was using the wrong paths for classpath and library path

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-NATIVE_DIR=`pwd`/native/`uname -m`
-CLASSPATH=jame.jar
+NATIVE_DIR=`pwd`/lib/native/`uname -m`
+CLASSPATH=lib/jame.jar
 MAIN=uk.co.nickthecoder.jame.test.TestVideo
 
 echo java -Djava.library.path=${NATIVE_DIR} -classpath "${CLASSPATH}" "${MAIN}"
